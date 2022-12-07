9233d91fc283a48931051c7866b593784a8c3fc2
sai-sreekhar <sai.sreekhar@gmail.com>
2022-12-07 06:33:12 +0530

    feat: update Readme

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
-#include  <iostream>
+#include <iostream>
 #include "Student.h"
 #include "Course.h"
 
 int main()
 {
-    Student* s1 = new Student();
+    Student *s1 = new Student();
     s1->addCourse(new Course("CS101", 'S', 1, 4), 1);
     s1->addCourse(new Course("CS102", 'S', 1, 4), 1);
     s1->addCourse(new Course("CS103", 'S', 1, 4), 1);
     s1->addCourse(new Course("CS104", 'A', 1, 4), 1);
     s1->addCourse(new Course("CS104", 'S', 2, 4), 2);
+    s1->addCourse(new Course("CS104", 'F', 3, 4), 3);
     s1->calculateCgpa();
-    std:: cout << s1->getCgpa();
+    std::cout << "CGPA: " << s1->getCgpa() << std::endl;
     s1->getSemsGpa();
-
 }
